cf1312370a65a7251d2da5fb1cc7f57cbf72bf3e
bansalyash24 <bansalyash066@gmail.com>
2023-07-23 20:04:25 +0530

    first

---
+++ App.js
@@ -0,0 +1,36 @@
+/*
+    <div>
+        <div id="child1">
+            <h1>I'm h1 tag</h1>
+            <h2>I'm h2 tag</h2>
+        </div>
+        <div id="child2">
+            <h1>I'm h1 tag</h1>
+            <h2>I'm h2 tag</h2>
+        </div>
+    </div>
+
+    ReactElement(object)=Html(browser Understandable)
+*/
+
+
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    [React.createElement("div", { id: "child1" },
+        [React.createElement("h1", {}, "I am an h1 tag"),
+        React.createElement("h2", {}, "I am a h2 tag")]
+    ),
+    React.createElement("div", { id: "child2" },
+        [React.createElement("h1", {}, "I am an h1 tag"),
+        React.createElement("h2", {}, "I am a h2 tag")]
+    )]
+)
+
+//JSX
+
+console.log(parent) //object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent)
